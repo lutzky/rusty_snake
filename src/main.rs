264a4f7bb63adef1c9a2cfd61ba6e31e7c2dd31b
Ohad Lutzky <ohad@lutzky.net>
2022-11-06 23:13:44 +0000

    Move game into a struct

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::time::{Duration, Instant};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use termion::{clear, cursor};
+use termion::{clear, cursor, AsyncReader};
 
 fn get_key(
     keys: &mut termion::input::Keys<termion::AsyncReader>,
@@ -27,95 +27,121 @@ fn get_key(
     }
 }
 
-fn game(args: Args) {
-    let stdin = termion::async_stdin();
+struct Game {
+    args: Args,
 
-    let mut keys = stdin.keys();
+    stdout: termion::raw::RawTerminal<std::io::StdoutLock<'static>>,
+    keys: termion::input::Keys<AsyncReader>,
 
-    let mut last_key: Option<Key> = None;
+    last_key: Option<Key>,
 
-    let mut x: u16 = args.field_width / 2;
-    let mut y: u16 = args.field_height / 2;
+    pos: (u16, u16),
+    tail_coords: VecDeque<(u16, u16)>,
 
-    let stdout = stdout();
-    let mut stdout = stdout.lock().into_raw_mode().unwrap();
+    direction: Key,
+    last_motion: std::time::Instant,
+}
 
-    let mut direction = Key::Right;
+fn gen_tail_coords(pos: (u16, u16), length: u16) -> VecDeque<(u16, u16)> {
+    let mut result = VecDeque::new();
 
-    let mut now = Instant::now();
-
-    let mut coords: VecDeque<(u16, u16)> = VecDeque::new();
-
-    // TODO(lutzky) collapse these?
-    for i in x - args.initial_snake_len..x {
-        coords.push_back((i, y));
-        print!("{}X", cursor::Goto(i, y));
+    for i in pos.0 - length..pos.0 {
+        result.push_back((i, pos.1));
     }
 
-    stdout.flush();
+    result
+}
 
-    const FRAME_DELAY: Duration = Duration::from_millis(5);
-    const MOTION_DELAY: Duration = Duration::from_millis(60);
+impl Game {
+    fn new(args: Args) -> Self {
+        let pos = (args.field_width / 2, args.field_height / 2);
+        Self {
+            stdout: stdout().lock().into_raw_mode().unwrap(),
+            keys: termion::async_stdin().keys(),
+            last_key: None,
+            pos,
+            tail_coords: gen_tail_coords(pos, args.initial_snake_len),
+            direction: Key::Right,
+            last_motion: Instant::now(),
+            args,
+        }
+    }
 
-    // TODO(lutzky): Drop me, debug only
-    let mut last_popped: (u16, u16) = (0, 0);
+    fn position_cursor(&self, pos: (u16, u16)) -> cursor::Goto {
+        cursor::Goto(pos.0 + 1, pos.1 + 2)
+    }
 
-    loop {
-        let k = get_key(&mut keys);
-        match k {
-            None => {}
-            Some(Err(_)) => {}
-            Some(Ok(k)) => last_key = Some(k),
+    fn play(mut self) -> Result<(), std::io::Error> {
+        // TODO(lutzky) collapse these?
+        for c in &self.tail_coords {
+            print!("{}X", self.position_cursor(*c));
         }
 
-        if now.elapsed() > MOTION_DELAY {
-            now = Instant::now();
-            match direction {
-                Key::Up => y = (y + args.field_height - 1) % args.field_height,
-                Key::Down => y = (y + 1) % args.field_height,
-                Key::Left => x = (x + args.field_width - 1) % args.field_width,
-                Key::Right => x = (x + 1) % args.field_width,
-                _ => todo!("use direction-specific enum"),
-            }
-            coords.push_back((x, y));
-            print!("{}X", cursor::Goto(x, y));
-            match coords.pop_front() {
+        self.stdout.flush()?;
+
+        const FRAME_DELAY: Duration = Duration::from_millis(5);
+        const MOTION_DELAY: Duration = Duration::from_millis(60);
+
+        // TODO(lutzky): Drop me, debug only
+        let mut last_popped: (u16, u16) = (0, 0);
+
+        loop {
+            let k = get_key(&mut self.keys);
+            match k {
                 None => {}
-                Some((x, y)) => {
-                    print!("{} ", cursor::Goto(x, y));
-                    last_popped = (x, y);
+                Some(Err(_)) => {}
+                Some(Ok(k)) => self.last_key = Some(k),
+            }
+
+            if self.last_motion.elapsed() > MOTION_DELAY {
+                self.last_motion = Instant::now();
+                match self.direction {
+                    Key::Up => self.pos.1 = (self.pos.1 + self.args.field_height - 1) % self.args.field_height,
+                    Key::Down => self.pos.1 = (self.pos.1 + 1) % self.args.field_height,
+                    Key::Left => self.pos.0 = (self.pos.0 + self.args.field_width - 1) % self.args.field_width,
+                    Key::Right => self.pos.0 = (self.pos.0 + 1) % self.args.field_width,
+                    _ => todo!("use direction-specific enum"),
                 }
-            };
+                self.tail_coords.push_back(self.pos);
+                print!("{}X", self.position_cursor(self.pos));
+                match self.tail_coords.pop_front() {
+                    None => {}
+                    Some(c) => {
+                        print!("{} ", self.position_cursor(c));
+                        last_popped = c;
+                    }
+                };
+            }
+
+            match k {
+                None => {}
+                Some(Ok(Key::Esc)) => return Ok(()),
+                Some(Err(e)) => panic!("panic! {}", e),
+
+                // TODO: Collapse these?
+                Some(Ok(Key::Up)) => self.direction = Key::Up,
+                Some(Ok(Key::Down)) => self.direction = Key::Down,
+                Some(Ok(Key::Left)) => self.direction = Key::Left,
+                Some(Ok(Key::Right)) => self.direction = Key::Right,
+
+                _ => {}
+            }
+
+            print!(
+                "{}{}Last key: {:?} pos: ({}, {}); last_popped: ({}, {})",
+                cursor::Goto(1, 1),
+                clear::CurrentLine,
+                self.last_key,
+                self.pos.0,
+                self.pos.1,
+                last_popped.0,
+                last_popped.1,
+            );
+
+            self.stdout.flush().expect("should be able to flush stdout");
+
+            std::thread::sleep(FRAME_DELAY);
         }
-
-        match k {
-            None => {}
-            Some(Ok(Key::Esc)) => return,
-            Some(Err(e)) => panic!("panic! {}", e),
-
-            // TODO: Collapse these?
-            Some(Ok(Key::Up)) => direction = Key::Up,
-            Some(Ok(Key::Down)) => direction = Key::Down,
-            Some(Ok(Key::Left)) => direction = Key::Left,
-            Some(Ok(Key::Right)) => direction = Key::Right,
-
-            _ => {}
-        }
-
-        print!(
-            "{}{}Last key: {:?} pos: ({}, {}); last_popped: ({}, {})",
-            cursor::Goto(1, 1),
-            clear::CurrentLine,
-            last_key,
-            x,
-            y,
-            last_popped.0,
-            last_popped.1,
-        );
-
-        stdout.flush().expect("should be able to flush stdout");
-
-        std::thread::sleep(FRAME_DELAY);
     }
 }
 
@@ -143,7 +169,7 @@ fn main() {
 
     print!("{}{}{}", clear::All, cursor::Hide, cursor::Goto(1, 1));
     stdout.flush().unwrap();
-    game(args);
+    Game::new(args).play().expect("game shouldn't error out");
     print!("{}{}{}", clear::All, cursor::Goto(1, 1), cursor::Show);
     stdout.flush().unwrap();
 }
